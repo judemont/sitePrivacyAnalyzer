e08b02c3e355e3c5c599f93aa594d264fd26c406
JdM <julien@rmbi.ch>
2025-01-01 20:17:06 +0100

    s

--- api/getRequests.php
+++ api/getRequests.php
@@ -12,7 +12,7 @@ if (filter_var($url, FILTER_VALIDATE_URL) === false) {
 }
 
 $return_var = null;
-exec('export NVM_DIR="$HOME/.nvm" [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" ');
+exec('export NVM_DIR="$HOME/.nvm" [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" ');
 
 $return_var = null;
 exec("node " . escapeshellarg($scriptPath) . " " . escapeshellarg($url) . " 2>&1", $output, $return_var);
